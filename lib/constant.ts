1c2bb8255a3a6074390f9d5d9c2fc2710c57f8c5
oyaro <hillaryoyaroh@gmail.com>
2025-04-08 11:25:59 +0300

    creation of data & constants

---
+++ lib/constant.ts
@@ -0,0 +1,5 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'EmaliExpress '
+export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || 'Spend less,enjoy more.'  
+export const APP_DESCRIPTION = 
+    process.env.NEXT_PUBLIC_APP_DESCRIPTION || 
+    'Marketplace app,build with Next.js, Tailwind CSS, and MongoDB'  
