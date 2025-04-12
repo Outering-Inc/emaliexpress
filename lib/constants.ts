2d41614ab3e2a9131d665df61bfb6e6f35ed14fb
oyaro <hillaryoyaroh@gmail.com>
2025-04-12 10:27:25 +0300

    #09-implement-product-gallery

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,3 +3,5 @@ export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || 'Spend less,enjo
 export const APP_DESCRIPTION = 
     process.env.NEXT_PUBLIC_APP_DESCRIPTION || 
     'Marketplace app,build with Next.js, Tailwind CSS, and MongoDB'  
+
+export const PAGE_SIZE = Number(process.env.NEXT_PUBLIC_PAGE_SIZE || 10)     
