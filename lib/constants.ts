528779cb358c6d07e69b463f5a76dabb63968a75
oyaro <hillaryoyaroh@gmail.com>
2025-04-20 14:06:07 +0300

    #16-update and radio-implementatio

--- lib/constants.ts
+++ lib/constants.ts
@@ -7,4 +7,45 @@ export const APP_DESCRIPTION =
 export const APP_COPYRIGHT = process.env.NEXT_PUBLIC_APP_COPYRIGHT || `Copyright © ${new Date().getFullYear()} ${APP_NAME}. All rights reserved.`    
 
 export const PAGE_SIZE = Number(process.env.NEXT_PUBLIC_PAGE_SIZE || 10)   
-export const FREE_SHIPPING_PRICE = Number(process.env.NEXT_PUBLIC_FREE_SHIPPING_PRICE || 35)  
+export const FREE_SHIPPING_PRICE = Number(process.env.NEXT_PUBLIC_FREE_SHIPPING_PRICE || 35) 
+
+export const AVAILABLE_PAYMENT_METHODS = [
+  {
+    name: 'PayPal',
+    commission: 0,
+    isDefault: true,
+  },
+  {
+    name: 'Stripe',
+    commission: 0,
+    isDefault: false,
+  },
+  {
+    name: 'Cash On Delivery',
+    commission: 0,
+    isDefault: false,
+  }
+]
+
+export const DEFAULT_PAYMENT_METHOD = process.env.DEFAULT_PAYMENT_METHOD || 'PayPal'
+
+export const AVAILABLE_DELIVERY_DATES = [
+    {
+        name: 'Tomorrow',
+        daysToDeliver: 1,
+        shippingPrice: 12.9,
+        freeShippingMinPrice: 0
+    },
+    {
+        name: 'Next 3 days',
+        daysToDeliver: 3,
+        shippingPrice: 6.9,
+        freeShippingMinPrice: 0
+    },
+    {
+        name: 'Next 5 days',
+        daysToDeliver: 5,
+        shippingPrice: 4.9,
+        freeShippingMinPrice: 35
+    }
+]
